bfd62f0218ff5b56415b9edcad44f0e5a9460c12
WilliamTuuu <t105590025@ntut.edu.tw>
2018-03-08 19:50:40 +0800

    777

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -6,6 +6,15 @@
 using namespace System;
 bool Guess(int number)
 {
+	if (number > target) {
+		std::cout << "Smaller" << std::endl;
+		return false;
+	}
+	else if (number < target) {
+		std::cout << "Bigger" << std::endl;
+		return false;
+	}
+	return true;
 }
 int main(array<System::String^>^ args)
 {
